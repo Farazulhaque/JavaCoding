bf4e904cde3bbd32ded2d018d0c607ba1b5d3552
Faraz <farazattari786@gmail.com>
2021-07-31 12:57:47 +0530

    split string separated by comma

---
+++ Q29/split_words.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class split_words {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter input string: ");
+		String str = scnr.nextLine();
+		while (str.charAt(0) != 'q') {
+			int flag = 0;
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == ',') {
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 1) {
+				String[] name = str.split(",");
+				System.out.println("First word: " + name[0]);
+				System.out.println("Second word: " + name[1]);
+			} else {
+				System.out.println("ERROR: No comma in a string");
+			}
+			System.out.println("\nEnter input string: ");
+			str = scnr.nextLine();
+		}
+	}
+}
