c5ebb7e4b5caa80f0b7ac29fa84fe78bf0fa35f1
Faraz <farazattari786@gmail.com>
2021-09-08 17:22:33 +0530

    convert decimal number to octal number in java

---
+++ Q38/DecimalToOctal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DecimalToOctal {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int octal = 0, i = 1, remainder;
+
+        // Input the decimal number
+        System.out.print("Enter a Decimal Number: ");
+        int decimal = in.nextInt();
+
+        // loop until the decimal number is not 0
+        while (decimal > 0) {
+            remainder = decimal % 8; // compute remainder
+            octal += i * remainder; // concatenate remainders to the octal
+            decimal = decimal / 8; // update decimal with quotient
+            i = i * 10; // multiply 10 to i
+        }
+
+        // output the octal number
+        System.out.println("Octal Number: " + octal);
+    }
+}
