c5a5626bb3d03d2a870896715da8d06f9e2d4519
Faraz <farazattari786@gmail.com>
2021-09-08 17:30:48 +0530

    Reverse a number in Java

---
+++ Q39/ReverseNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int num = sc.nextInt();
+        int reversed = 0;
+
+        // run loop until num becomes 0
+        while (num != 0) {
+
+            // get last digit from num
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+
+            // remove the last digit from num
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
