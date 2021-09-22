72182e0ef12ac6f97050e18346bae9a00b637c61
Faraz <farazattari786@gmail.com>
2021-09-23 00:30:52 +0530

    volume of cylinder

---
+++ Q49/VolumeCylinder.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VolumeCylinder {
+    static final double PI = 3.1415926536;
+
+    public static void main(String[] args) {
+        double radius;
+        double height;
+        double volume;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius of cylinder: ");
+        radius = sc.nextDouble();
+        System.out.print("Enter height of cylinder: ");
+        height = sc.nextDouble();
+
+        volume = PI * radius * radius * height;
+
+        System.out.println("\nRadius: " + radius);
+        System.out.println("Height: " + height);
+        System.out.println("Volume: " + volume);
+    }
+}
