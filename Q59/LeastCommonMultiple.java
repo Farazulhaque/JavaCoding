541f058fff8d989cac19abf7c9d4f5f3209ce7f1
Farazulhaque <farazattari786@gmail.com>
2021-10-16 21:43:22 +0530

    find lcm of two numbers

---
+++ Q59/LeastCommonMultiple.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LeastCommonMultiple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int num2 = sc.nextInt();
+        int greater;
+        if (num1 > num2) {
+            greater = num1;
+        } else {
+            greater = num2;
+        }
+        while (true) {
+            if (greater % num1 == 0 && greater % num2 == 0) {
+                break;
+            }
+            greater += 1;
+        }
+        int lcm = greater;
+        System.out.print("LCM of " + num1 + " and " + num2 + " is " + lcm);
+
+        sc.close();
+    }
+}
+
