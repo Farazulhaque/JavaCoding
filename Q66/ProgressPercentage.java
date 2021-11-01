4a6c6f930313c2b09fa12a1cac41930a4e40d533
Farazulhaque <farazattari786@gmail.com>
2021-11-01 12:09:52 +0530

    dummy progress percentage

---
+++ Q66/ProgressPercentage.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+public class ProgressPercentage {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.print("\rCurrent Progress: " + i + "%");
+            // try{
+            // Thread.sleep(20);
+            // }
+            // catch (InterruptedException e){
+            // e.printStackTrace();
+            // }
+
+            for (long j = 0; j < 400000000; j++)
+                ;
+        }
+        System.out.println("Download Completed");
+    }
+}
