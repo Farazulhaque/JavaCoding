c9a124cc5376f53e2e648e0f61cf878297776b1c
Farazulhaque <farazattari786@gmail.com>
2022-09-18 21:04:07 +0530

    fixing error

--- Q110/Main.java
+++ Q110/Main.java
@@ -1,40 +1,36 @@
-package Q110;
+// package Q110;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.stream.IntStream;
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStream;
+// import java.io.InputStreamReader;
+// import java.util.List;
+// import java.util.stream.IntStream;
 
-public class Main {
-	public static void main(String[] args) {
-		BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
-		
-		int postDetailsCount=Integer.parseInt(bufferedReader.readLine().trim());
-		
-		List<String> postDetails =IntStream.range(0, postDetailsCount).mapToObj(i->{
-			
-			
-			
-			try {
-				return bufferedReader.readLine();
-			}catch (IOException ex) {
-				throw new RuntimeException(ex);
-			}
-		})
-			.collect(Tolist());
-			
-			
-		
-			Result.findMostPopularTopic(postDetails);
-			bufferedReader.close();
-		
-		
-		}
-	}
+// public class Main {
+// public static void main(String[] args) {
+// BufferedReader bufferedReader=new BufferedReader(new
+// InputStreamReader(System.in));
 
-	public void findMostPopularTopic(List<String> post){
-			 
-	}
-}
+// int postDetailsCount=Integer.parseInt(bufferedReader.readLine().trim());
+
+// List<String> postDetails =IntStream.range(0, postDetailsCount).mapToObj(i->{
+
+// try {
+// return bufferedReader.readLine();
+// }catch (IOException ex) {
+// throw new RuntimeException(ex);
+// }
+// })
+// .collect(Tolist());
+
+// Result.findMostPopularTopic(postDetails);
+// bufferedReader.close();
+
+// }
+// }
+
+// public void findMostPopularTopic(List<String> post){
+
+// }
+// }
