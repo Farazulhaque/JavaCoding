9dce71ec0719c39117c6071d2c4e87d28fadb41d
Farazulhaque <farazattari786@gmail.com>
2022-09-25 20:39:15 +0530

    getting number of attempts it will take to get the max value in a dice roll

---
+++ Q112/Boxcars.java
@@ -0,0 +1,17 @@
+public class Boxcars {
+	public static void main(String[] args) {
+		int dice1 = 0;
+		int dice2 = 0;
+		dice1 = 1 + (int) (Math.random() * 6);
+		dice2 = 1 + (int) (Math.random() * 6);
+		System.out.println("Rolled " + dice1 + ", " + dice2);
+		int attempt = 1;
+		while (dice1 != 6 || dice2 != 6) {
+			dice1 = 1 + (int) (Math.random() * 6);
+			dice2 = 1 + (int) (Math.random() * 6);
+			System.out.println("Rolled " + dice1 + ", " + dice2);
+			attempt++;
+		}
+		System.out.print("It took " + attempt + " attempts to roll a 12");
+	}
+}
